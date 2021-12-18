927fc761c7b8820fca19c82f9cdfb6665d94ef2b
Rakibul Hassan Rony <rakibulhassan.rony123@gmail.com>
2021-12-18 21:00:57 +0600

    fizz buzz =  leetcode

--- LeetCode/src/com/rony/Main.java
+++ LeetCode/src/com/rony/Main.java
@@ -1,20 +1,46 @@
 package com.rony;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.trailingZeroes(30));
+        System.out.println(solution.fizzBuzz(5));
     }
 }
 
 class Solution {
     public int trailingZeroes(int n) {
-        // Factorial Trailing Zeroes
+        /**
+         * Factorial Trailing Zeroes
+         *  counting the number of 5's in the number
+         */
         int count = 0;
         while (n>0){
             n/=5;
             count += n;
         }
         return count;
+    }
+    public List<String> fizzBuzz(int n) {
+        /**
+         * fizz buzz
+         */
+        List<String> result = new ArrayList<>();
+        for(int i=1; i<=n; i++){
+            if(i%3==0 && i%5==0){
+                result.add("FizzBuzz");
+            }
+            else if(i%3==0){
+                result.add("Fizz");
+            }else if(i%5==0){
+                result.add("Buzz");
+            }else {
+                result.add(Integer.toString(i));
+            }
+        }
+        return result;
+
     }
 }
