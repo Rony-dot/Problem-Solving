4b40de194a430aa55feb4b7e598684d11535d7ca
Rakibul Hassan Rony <rakibul.hassan@dsinnovators.com>
2022-08-05 20:27:27 +0600

    CF solve

--- Codeforces/1714_A_Everyone_Loves_to_Sleep.cpp
+++ Codeforces/1714_A_Everyone_Loves_to_Sleep.cpp
@@ -4,7 +4,6 @@
 #define sz(v) (int)v.size()
 #define all(v) v.begin(),v.end()
 #define len(s) (int)s.size()
-
 using namespace std;
 
 const int inf = 1e9 + 7;
@@ -14,44 +13,49 @@ void solve(){
     int n, H, M;
     cin >> n >> H >> M;
 
-    vector<pair<int,int> > vect;
+    vector<int> vect;
     int h, m;
     forn(i, n) {
         cin >> h >> m;
-        vect.push_back(make_pair(h,m));
+        vect.push_back(h*60+m);
     }
-    cout<<H<<" = "<<M<<endl;
     sort(all(vect));
-    h = vect[0].first;
-    m = vect[0].second;
     int finalH=0, finalM=0;
     int sleepTime = H*60 + M;
-    int alarmTime = h*60 + m;
+    int alarmTime = 0, flag=0;
+    for(int i=0; i<sz(vect); i++){
+        if(vect[i]>=sleepTime){
+            alarmTime = vect[i];
+            flag = i;
+            break;
+        }
+    }
+    alarmTime = vect[flag]; 
+    
     int resultTime = 0;
     if(sleepTime < alarmTime){
         resultTime = alarmTime - sleepTime;
-        int tmp = resultTime / 60;
-        resultTime -= tmp * 60;
-        if (resultTime > 60 ){
-            finalH = resultTime / 60;
-            finalM = resultTime - (resultTime*finalH);
-            cout<<finalH << " : "<<finalM<<endl;
-        }else{
-            cout<<resultTime<<endl;
-        }
-    }else if(sleepTime > alarmTime){
-        
-    }
+        finalH = (int) (resultTime/60);
+        finalM = (int) (resultTime%60);
+        cout<< finalH <<" " <<finalM<<endl;
 
-    // cout<<finalH <<" "  << finalM<<endl;
+    }else if(sleepTime > alarmTime){
+        resultTime = (24*60) - sleepTime;
+        resultTime += alarmTime;
+        finalH = (int) (resultTime/60);
+        finalM = (int) (resultTime%60);
+        cout<< finalH <<" " <<finalM<<endl;
+    }else{
+        cout<<"0 0"<<endl;
+    }
 
 }
  
 
 int main() {
-    // push from stage
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
+
+    // freopen("input.txt","r",stdin);
+    // freopen("output.txt","w",stdout);
     int t;
     cin >> t;
     forn(tt, t) {
