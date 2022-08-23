95414aae76ed4995964c5f6d1e3b98babbf7d7cb
Rony-dot <rakibulhassan.rony123@gmail.com>
2022-08-23 20:39:52 +0600

    office lc

---
+++ LeetCode/src/com/rony/palindrome_linked_list_234/Solution.java
@@ -0,0 +1,19 @@
+package com.rony.palindrome_linked_list_234;
+
+
+public class Solution {
+    public boolean isPalindrome(ListNode head) {
+        boolean ans = false;
+
+
+        return ans;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
