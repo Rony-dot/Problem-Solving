8d45669d6d30a471d064ba1913ef7359d6b1db5f
Rakibul Hassan Rony <rakibulhassan.rony123@gmail.com>
2023-03-10 20:35:22 +0600

    leetcode 121 Easy

--- LeetCode/src/com/rony/best_time_to_buy_sell_stock_121/Main.java
+++ LeetCode/src/com/rony/best_time_to_buy_sell_stock_121/Main.java
@@ -1,7 +1,6 @@
 package com.rony.best_time_to_buy_sell_stock_121;
 
 import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
@@ -12,11 +11,14 @@ public class Main {
 
 
 class Solution {
+// TODO: time O(n) cause even though we have two pointers we actually traverse the array only once
+// TODO: space O(1) cause we only used some pointer,
     public int maxProfit(int[] prices) {
-        int l = 0;
-        int r = 1;
+        int l = 0;  // left=buy
+        int r = 1;  // right=sell
         int maxP = 0;
         while(r < prices.length){
+            // profitable ?
             if(prices[l]<prices[r]){
                 int profit = prices[r] - prices[l];
                 maxP = Math.max(profit,maxP);
