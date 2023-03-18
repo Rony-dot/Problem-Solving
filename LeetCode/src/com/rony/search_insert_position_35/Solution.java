e5cf3913f4131d1f4009e70d489042fd79747350
Rakibul Hassan Rony <rakibulhassan.rony123@gmail.com>
2023-03-18 14:21:17 +0600

    leetcode 35 Easy

--- LeetCode/src/com/rony/search_insert_position_35/Solution.java
+++ LeetCode/src/com/rony/search_insert_position_35/Solution.java
@@ -3,8 +3,28 @@ package com.rony.search_insert_position_35;
 public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
+        System.out.println(1/2);
+        System.out.println(solution.searchInsert(new int[]{1,3,5,6},0));
+        System.out.println(solution.binarySearchInsert(new int[]{1,3,5,6},0));
     }
-
+    public int binarySearchInsert(int[] nums, int target){
+        int lo = 0;
+        int hi = nums.length-1;
+        int mid = 0;
+        while(lo<=hi){
+//            mid = (lo+hi)/2;
+            mid = (lo+hi) >>> 1; //same as divide by 2
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                lo = mid+1;
+            }else{
+                hi = mid-1;
+            }
+        }
+        return lo;
+    }
     public int searchInsert(int[] nums, int target) {
 
         for(int i=0; i<nums.length; i++){
@@ -13,4 +33,6 @@ public class Solution {
         }
         return nums.length;
     }
+
+
 }
